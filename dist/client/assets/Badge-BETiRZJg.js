53f1a33d968c825b42606c5f0efaf710e492eeff
jonsherrard <jon@devular.com>
2026-03-26 09:26:45 -0600

    remove dist

--- dist/client/assets/Badge-BETiRZJg.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./preload-helper-r4tpkvBZ.js";var t=e();function n({variant:e,children:n}){return(0,t.jsx)(`span`,{className:`badge badge-${e}`,children:n})}export{n as t};
--- dist/server/assets/start-CHEQZ8CA.js
+++
@@ -1,4 +0,0 @@
-//#region node_modules/.pnpm/@tanstack+react-start@1.167.3_react-dom@19.2.4_react@19.2.4__react@19.2.4_vite@8.0.1_@t_b82ef8f29736859611a1647491e75a04/node_modules/@tanstack/react-start/dist/plugin/default-entry/start.ts
-var startInstance = void 0;
-//#endregion
-export { startInstance };
